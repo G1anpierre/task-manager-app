af382b370ca471e179f3dc4d79467f08f086e331
Gianpierre Fernandez <fgianpierre@gmail.com>
2023-08-28 09:57:01 +0200

    first implementation of graphic and project setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      mantis: {
+        50: '#f6faf3',
+        100: '#e9f5e3',
+        200: '#d3eac8',
+        300: '#afd89d',
+        400: '#82bd69',
+        500: '#61a146',
+        600: '#4c8435',
+        700: '#3d692c',
+        800: '#345427',
+        900: '#2b4522',
+        950: '#13250e',
+      },
+    },
+  },
+  plugins: [],
+}
